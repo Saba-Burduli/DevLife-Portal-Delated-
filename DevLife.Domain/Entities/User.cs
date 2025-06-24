e4c7f181b9516e1d78a43dbf6a0965743b0ee106
Saba-Burduli <sabagg790@gmail.com>
2025-06-24 04:27:14 +0400

    Fix typo and extend User entity with new properties and relations
    Corrected the typo in 'ExperienceLevel' property name. Added 'CreatedAt' and 'LastLogonAt' for tracking user activity. Introduced relationships with GameSession, CasinoBet, and CodeSubmission for expanded functionality.

--- DevLife.Domain/Entities/User.cs
+++ DevLife.Domain/Entities/User.cs
@@ -9,6 +9,19 @@ public class User
     public DateTime DateOfBirt { get; set; }
     public ZodiacSign ZodiacSign { get; set; }
     public TechStack TechStack { get; set; }
-    public ExperieceLevel Experience { get; set; }
+    public ExperienceLevel Experience { get; set; }
     public int Points { get; set; }
+    public DateTime? CreatedAt { get; set; }
+    public DateTime? LastLogonAt { get; set; }
+
+    //Relations :
+    
+    //User => GameSession
+    public List<GameSession> GameSessions { get; set; } = new();
+    
+    //User => CasinoBet
+    public List<CasinoBet> CasinoBets { get; set; } = new();
+    
+    //User => CodeSubmission
+    public List<CodeSubmission> CodeSubmissions { get; set; } = new();
 }
