2eedc0b6422c1dbb9bbe82709ecb750f0e48e6f1
Saba-Burduli <sabagg790@gmail.com>
2025-06-24 04:25:30 +0400

    Add CasinoBet entity to domain model
    Introduce the CasinoBet class representing user bets in casino games. Includes properties for user, game, bet details, outcome, and relations to User and CasinoGame entities. This sets the foundation for handling casino-related logic.

---
+++ DevLife.Domain/Entities/CasinoBet.cs
@@ -0,0 +1,21 @@
+﻿namespace DevLife.Domain.Entities;
+
+public class CasinoBet
+{
+    public int CasinoBetId { get; set; }
+    public int UserId { get; set; }
+    public int GameId { get; set; }
+    public int BetAmount { get; set; }
+    public int GuessedAnswer { get; set; }
+    public bool IsCorrect { get; set; }
+    public int PointsWon { get; set; }
+    public DateTime PlacedAt { get; set; }
+        
+    // Relations :
+    
+    // CasinoBet => User
+    public virtual User? User { get; set; }
+    
+    // CasinoBet => CasinoGame
+    public virtual CasinoGame? Game { get; set; }
+}
