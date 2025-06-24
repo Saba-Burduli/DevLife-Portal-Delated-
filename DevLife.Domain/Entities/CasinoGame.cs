1e4fbb0d318074277e68d1460c9d02b04a6da0d7
Saba-Burduli <sabagg790@gmail.com>
2025-06-24 04:25:36 +0400

    Refactor CasinoGame entity by adding new properties.
    Introduce new fields such as Title, Explanation1, Explanation2, IsActive, and CreatedAt to enhance the entity's functionality and data representation. Renamed DifficultyLevel to Difficulty for naming consistency. Added necessary using directives for future enhancements.

--- DevLife.Domain/Entities/CasinoGame.cs
+++ DevLife.Domain/Entities/CasinoGame.cs
@@ -1,11 +1,20 @@
-﻿namespace DevLife.Domain.Entities;
+﻿using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using Microsoft.VisualBasic;
+
+namespace DevLife.Domain.Entities;
 
 public class CasinoGame
 {
     public int CasinoGameId { get; set; }
-    public string? CodeSnippet1  { get; set; }
-    public string? CodeSnippet2  { get; set; }
-    public int CorrectAnswer  { get; set; }
+    public string? Title { get; set; }
+    public string? CodeSnippet1 { get; set; }
+    public string? CodeSnippet2 { get; set; }
+    public string? Explanation1 { get; set; }
+    public string? Explanation2 { get; set; }
+    public int CorrectAnswer { get; set; } // 1 or 2
     public TechStack TechStack { get; set; }
-    public DifficultyLevel DifficultyLevel { get; set; }
+    public DifficultyLevel Difficulty { get; set; }
+    public bool IsActive { get; set; }
+    public DateTime CreatedAt { get; set; }
 }
