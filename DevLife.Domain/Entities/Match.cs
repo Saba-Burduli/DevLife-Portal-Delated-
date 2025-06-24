49616b0ac2b2d15ef5dd5a1986255775b02b124f
Saba-Burduli <sabagg790@gmail.com>
2025-06-24 04:27:09 +0400

    Add Match entity to domain model
    Introduced the Match entity to represent user interactions like likes and matches. This includes properties for relationships with User and DeveloperProfile entities, along with match-specific attributes such as IsLiked and MatchedAt. This helps establish a foundation for matchmaking functionality.

---
+++ DevLife.Domain/Entities/Match.cs
@@ -0,0 +1,20 @@
+﻿namespace DevLife.Domain.Entities;
+
+public class Match
+{
+    public int MatchId { get; set; }
+    public bool IsLiked { get; set; }
+    public bool IsMatched { get; set; }
+    public DateTime MatchedAt { get; set; }
+
+    // Relations :
+    
+    //Match => User
+    public int UserId { get; set; }
+    public virtual User? User { get; set; }
+
+    // Match => DeveloperProfile
+    public int ProfileId { get; set; }
+    public virtual DeveloperProfile? Profile { get; set; }
+}
+
