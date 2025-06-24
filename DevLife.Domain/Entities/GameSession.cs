02824008aba78c06bd33e688b70a3a89d5a03d0d
Saba-Burduli <sabagg790@gmail.com>
2025-06-24 04:26:59 +0400

    Add new properties and relationships to GameSession entity
    Introduced additional properties like Distance, BugsAvoided, DeadlinesAvoided, CoffeeCollected, and SessionId for extended session tracking. Added data annotations for primary key and foreign key relationships, linking GameSession to the User entity. These updates enhance the entity's functionality and database integration.

--- DevLife.Domain/Entities/GameSession.cs
+++ DevLife.Domain/Entities/GameSession.cs
@@ -1,11 +1,26 @@
-﻿namespace DevLife.Domain.Entities;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DevLife.Domain.Entities;
 
 public class GameSession
 {
+    [Key]
     public int GameSessionId { get; set; }
-    public int UserId { get; set; }
     public int Score { get; set; }
+    public int Distance { get; set; }
+    public int BugsAvoided { get; set; }
+    public int DeadlinesAvoided { get; set; }
+    public int CoffeeCollected { get; set; }
     public DateTime StartTime { get; set; }
     public DateTime? EndTime { get; set; }
     public bool IsActive { get; set; }
+    public string? SessionId { get; set; }
+        
+    // Relations :
+    
+    //GameSession => User
+    [ForeignKey("User")]
+    public int UserId { get; set; }
+    public virtual User? User { get; set; }
 }
