43cd8aff5b985c00b1970ac95fe2cb669e626d62
Saba-Burduli <sabagg790@gmail.com>
2025-06-24 04:25:47 +0400

    Add CodeSubmission entity to domain model
    Introduces the CodeSubmission class to represent code submission details such as code, language, execution time, score, and relations to User and CodeChallenge. This enhances the domain model to support submissions and associated metadata.

---
+++ DevLife.Domain/Entities/CodeSubmission.cs
@@ -0,0 +1,23 @@
+﻿namespace DevLife.Domain.Entities;
+
+public class CodeSubmission
+{
+    public int CodeSubmissionId { get; set; }
+    public string Code { get; set; }
+    public string Language { get; set; }
+    public bool IsCorrect { get; set; }
+    public int ExecutionTime { get; set; }
+    public string RoastFeedback { get; set; }
+    public int Score { get; set; }
+    public DateTime SubmittedAt { get; set; }
+
+    // Relations :
+
+    //CodeSubmission => User
+    public int UserId { get; set; }
+    public virtual User? User { get; set; }
+
+    //CodeSubmission => CodeChallenge
+    public int ChallengeId { get; set; }
+    public virtual CodeChallenge? Challenge { get; set; }
+}
